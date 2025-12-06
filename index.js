3f1e54d01559104acb12dc39d61fd93712530d7c
RUKUNDOChristian <chrissbateur@gmail.com>
2025-12-07 00:32:22 +0200

    Fix code formatting for /about route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  
   if (req.url === '/about') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
